81363c4b0ddefa0644926c94934e19a5287f0c4e
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-02-06 19:56:11 +0900

    sql: 소마 대비 SQL Kit 1일차

---
+++ sql/133025.sql
@@ -0,0 +1,7 @@
+-- 과일로 만든 아이스크림 고르기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133025
+
+SELECT DISTINCT F.FLAVOR 
+FROM FIRST_HALF as F JOIN ICECREAM_INFO as I ON F.FLAVOR = I.FLAVOR 
+WHERE TOTAL_ORDER > 3000 AND INGREDIENT_TYPE = 'fruit_based'
+ORDER BY TOTAL_ORDER DESC;
