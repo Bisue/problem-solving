d278d16f81ffdd7ea7fa829da5b51902276ee0fd
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-02-08 10:50:44 +0900

    fix: 406.py 오타 수정

--- 2024-dgu-spring-camp/12/406.py
+++ 2024-dgu-spring-camp/12/406.py
@@ -6,10 +6,8 @@ nums = list(map(int, input().split()))
 
 print(' '.join(map(str, nums)))
 for i in range(N):
-	pos = i
-	for j in range(N-i-1):
-		# print(i, (j, j+1), nums[j], nums[j+1])
-		if nums[pos] > nums[j+1]:
-			nums[pos], nums[j+1] = nums[j+1], nums[pos]
-			poos
-			print(' '.join(map(str, nums)))
+    for j in range(N-i-1):
+        # print(i, (j, j+1), nums[j], nums[j+1])
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            print(' '.join(map(str, nums)))
